752ebb2c5bb68d3d5ad0bce257bdd3203a8967b1
David Tran <frozenpho@gmail.com>
2021-11-10 09:33:22 -0700

    Initial commit

---
+++ src/apiCalls.js
@@ -0,0 +1,30 @@
+// const fetchData = (data) => {
+//   return fetch(`http://localhost:3001/api/v1/${data}`)
+//     .then(response => response.json())
+// }
+
+const getAllTravelers = () => {
+  return fetch('http://localhost:3001/api/v1/travelers')
+    .then(response => response.json())
+}
+
+const getSingleTraveler = (id) => {
+  return fetch(`http://localhost:3001/api/v1/travelers/${id}`)
+    .then(response => response.json())
+}
+
+const getAllTrips = () => {
+  return fetch('http://localhost:3001/api/v1/trips')
+    .then(response => response.json())
+}
+
+const getAllDestinations = () => {
+	return fetch('http://localhost:3001/api/v1/destinations')
+	  .then(response => response.json())
+}
+
+const getAllDestinations = () => {
+	return fetch('http://localhost:3001/api/v1/destinations')
+	  .then(response => response.json())
+}
+
